ac3573388ad11c1c3231d8e2d309b21c491bb48f
천수승 <cheonss2357@gmail.com>
2022-01-20 00:41:45 +0900

    21.01.19 10866 sys.stdin.readline으로 시간단축 성공(블로그 참고)

--- 10866.py
+++ 10866.py
@@ -1,42 +1,42 @@
 import sys
 sys.stdin = open('10866.txt', 'r')
+input = sys.stdin.readline
 from collections import deque
 N = int(input())
 arr = []
 queue = deque()
 for i in range(N):
     arr = input().split()
-    k = arr[0]
-    if k == "push_back":
+    if arr[0] == "push_back":
         queue.append(int(arr[1]))
-    elif k == "push_front":
+    elif arr[0] == "push_front":
         queue.appendleft(int(arr[1]))
-    elif k == 'pop_front':
+    elif arr[0] == 'pop_front':
         if len(queue):
-            q = queue.popleft()
-            print(q)
+            print(queue.popleft())
         else:
             print(-1)
-    elif k == 'pop_back':
+    elif arr[0] == 'pop_back':
         if len(queue):
-            q = queue.pop()
-            print(q)
+            print(queue.pop())
         else:
             print(-1)
-    elif k == 'size':
+    elif arr[0] == 'size':
         print(len(queue))
-    elif k == 'empty':
+    elif arr[0] == 'empty':
         if len(queue):
             print(0)
         else:
             print(1)
-    elif k == 'front':
+    elif arr[0] == 'front':
         if len(queue):
             print(queue[0])
         else:
             print(-1)
-    elif k == 'back':
+    elif arr[0] == 'back':
         if len(queue):
             print(queue[-1])
         else:
             print(-1)
+
+# 시간 단축 참조: https://yongku.tistory.com/entry/%EB%B0%B1%EC%A4%80-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EB%B0%B1%EC%A4%80-10866%EB%B2%88-%EB%8D%B1-%ED%8C%8C%EC%9D%B4%EC%8D%ACPython
