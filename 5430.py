66ea87aa9c87ce36b4523f3be4c0c2624da89183
천수승 <cheonss2357@gmail.com>
2022-01-26 00:24:09 +0900

    21.01.25 출력 형식에서 공백없애야할듯

---
+++ 5430.py
@@ -0,0 +1,46 @@
+import sys
+from collections import deque
+sys.stdin = open('5430.txt', 'r')
+queue = deque()
+N = int(input())
+for i in range(N):
+    func = input()
+    leng = int(input())
+    arr = input()
+    # print(arr)
+    arr = arr[1:-1]
+    arr = arr.split(',')
+    if leng != 0:
+        arr = deque(int(arr[i]) for i in range(len(arr)))
+    else:
+        arr = []
+    # print(func)
+    # print(leng)
+    # print('arr', arr)
+
+    cnt = 0
+    err = False
+    for j in range(len(func)):
+        if func[j] == 'R':
+            cnt += 1
+        else:
+            if cnt%2:
+                if arr:
+                    arr.pop()
+                else:
+                    print('error')
+                    err = True
+                    break
+            else:
+                if arr:
+                    arr.popleft()
+                else:
+                    print('error')
+                    err = True
+                    break
+
+    if cnt%2 == 1:
+        arr.reverse()
+    if err == False:
+        arr = list(arr)
+        print(arr)
