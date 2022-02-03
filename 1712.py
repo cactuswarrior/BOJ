49dc2fda26b4c22e047731e86676d15f4ce3b8c9
천수승 <cheonss2357@gmail.com>
2022-02-04 00:06:39 +0900

    22.02.04 반례 찾음

--- 1712.py
+++ 1712.py
@@ -1,10 +1,12 @@
 import sys
 sys.stdin = open('1712.txt', 'r')
 
+import math
 a, b, c = input().split()
 a, b, c = int(a), int(b), int(c)
-i = 1
-if a/(c-b) <= 0:
+if c <= b:
     print(-1)
+elif a/(c-b) < 1:
+    print(1)
 else:
-    print(round(a/(c-b) + 1))
+    print((a//(c-b)) + 1)
