a5fa3c8f8e072b10976caace3a32b16979a7b7b4
천수승 <cheonss2357@gmail.com>
2022-03-15 23:17:43 +0900

    22.03.15 1743도 품

--- 1743.py
+++ 1743.py
@@ -3,7 +3,7 @@ sys.stdin = open('1743.txt', 'r')
 from collections import deque
 # N 세로 M 가로 K 쓰레기 갯수
 N, M, K = map(int, input().split())
-print(N, M, K)
+# print(N, M, K)
 # 그래프
 graph = list([0]*(M+1) for _ in range(N+1))
 # 음식물 위치
@@ -11,7 +11,7 @@ for a in range(K):
     r, c = map(int, input().split())
     graph[r][c] = 1
 
-print(graph)
+# print(graph)
 
 dr = [0, 0, 1, -1]
 dc = [-1, 1, 0, 0]
@@ -23,6 +23,7 @@ def bfs(r, c):
     q = deque()
     q.append((r, c))
     graph[r][c] = 0
+    temp = 1
 
     while q:
         r, c = q.popleft()
@@ -30,18 +31,19 @@ def bfs(r, c):
             nr = r + dr[i]
             nc = c + dc[i]
 
-            if 0 <= nr < N and 0 <= nc < M and graph[nr][nc] == 1:
+            if 0 < nr <= N and 0 < nc <= M and graph[nr][nc] == 1: #나는 그래프 위치와 인덱스를 맞추기 위해 한 줄씩 더하므로 0을 포함하지 않고 N은 포함해야
                 q.append((nr, nc))
                 graph[nr][nc] = 0
                 temp += 1
 
-    return
+    if temp > cnt:
+        cnt = temp
 
-# 그래프 전체 순회
-for i in range(1,N+1):
+# 그래프 전체 순회함
     for j in range(1, M+1):
         if graph[i][j] == 1:
             bfs(i, j)
+            # print(temp)
 
 print(cnt)
 
